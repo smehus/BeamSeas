1b716ed33907ec6686ccfb73e7c3bdeb7145d2fc
Scott Mehus <mehus.scott@gmail.com>
2021-09-04 18:03:47 -0600

    i guess it is spinning around scaffolding

--- BeamSeas Shared/Nodes/Water/Terrain.swift
+++ BeamSeas Shared/Nodes/Water/Terrain.swift
@@ -18,7 +18,7 @@ class Terrain: Node {
     static var primarySlopeMap: MTLTexture!
     static var secondarySlopeMap: MTLTexture!
 
-    static let terrainSize: Float = 300
+    static let terrainSize: Float = 50
     
     static var terrainParams = TerrainParams(
         size: [Terrain.terrainSize, Terrain.terrainSize],
