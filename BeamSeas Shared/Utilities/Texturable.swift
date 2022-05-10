e5d8198b19fca1d5b8aa6950895d2dfc8839f06f
Scott Mehus <mehus.scott@gmail.com>
2022-05-10 13:12:11 -0600

    stuff

--- BeamSeas Shared/Utilities/Texturable.swift
+++ BeamSeas Shared/Utilities/Texturable.swift
@@ -95,12 +95,20 @@ extension Texturable {
 //                                   "negz.jpg"],
 //                           options: options)
         
-        return loadCubeMap(names: ["ridged_+x.png",
-                                   "ridged_-x.png",
-                                   "ridged_+y.png",
-                                   "ridged_-y.png",
-                                   "ridged_+z.png",
-                                   "ridged_-z.png"],
+//        return loadCubeMap(names: ["ridged_+x.png",
+//                                   "ridged_-x.png",
+//                                   "ridged_+y.png",
+//                                   "ridged_-y.png",
+//                                   "ridged_+z.png",
+//                                   "ridged_-z.png"],
+//                           options: options)
+        
+        return loadCubeMap(names: ["world_map_+x.png",
+                                   "world_map_-x.png",
+                                   "world_map_+y.png",
+                                   "world_map_-y.png",
+                                   "world_map_+z.png",
+                                   "world_map_-z.png"],
                            options: options)
     }
 }
